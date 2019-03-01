d2b1e11ff1b962022f48d60c947fb42c65128418
fucangyu <cangyufu@gmail.com>
2019-03-02 00:09:16 +0800

    add xici and others

---
+++ ipfeeder/cronjobs/validators/validators.py
@@ -0,0 +1,87 @@
+import logging
+from urllib.parse import urlparse
+
+import gevent
+import requests
+from cronjob.apps import BaseJob
+from cronjob.settings import settings
+from gevent import hub
+
+from ipfeeder.db import db
+
+hub.Hub.NOT_ERROR = (Exception, )
+
+LOGGER = logging.getLogger(__name__)
+
+
+def _validate(validate_urls, proxy_url, protocol) -> bool:
+    threads = [
+        gevent.spawn(
+            requests.get,
+            url,
+            proxies={protocol: proxy_url},
+            timeout=10,
+        ) for url in validate_urls
+    ]
+    gevent.joinall(threads)
+    for item in threads:
+        response = item.value
+        if response and response.ok:
+            resp = response.json()
+            origin = resp.get('origin', '').split(',')
+            return origin[0] == urlparse(proxy_url).hostname
+    return False
+
+
+def validate(url) -> bool:
+    try:
+        if url.startswith('https'):
+            return _validate(settings.VALIATE_HTTPS_URLS, url, 'https')
+        elif url.startswith('http'):
+            return _validate(settings.VALIATE_HTTP_URLS, url, 'http')
+        else:
+            return False
+    except Exception as e:
+        return False
+
+
+def _run(validator):
+    for ip in validator.get_value_func()():
+        if not ip:
+            continue
+        if validate(ip):
+            LOGGER.info(f'pass validator: {ip}')
+            db.add_validated(ip)
+
+
+class RawValidator(BaseJob):
+    rule = '5m'
+    right_now = True
+
+    def get_value_func(self):
+        return getattr(db, 'raw_iter')
+
+    def run(self):
+        gevent.sleep(30)
+        LOGGER.info('raw checker starting....')
+        _run(self)
+
+
+class HttpValidator(BaseJob):
+    rule = '20m'
+    right_now = False
+
+    def get_value_func(self):
+        return getattr(db, 'http_iter')
+
+    run = _run
+
+
+class HttpsValidator(BaseJob):
+    rule = '20m'
+    right_now = False
+
+    def get_value_func(self):
+        return getattr(db, 'https_iter')
+
+    run = _run
---
+++ ipfeeder/db.py
@@ -0,0 +1,55 @@
+from functools import partial
+
+from cronjob.settings import settings
+from redis import StrictRedis
+
+RAW_IPS = 'raw_proxy_ip'
+VALIDATED_HTTP = 'valicated_http_ip'
+VALIDATED_HTTPS = 'valicated_https_ip'
+
+
+class DB:
+    def __init__(self, **kwargs):
+        self.conn = StrictRedis(**kwargs)
+
+    @classmethod
+    def from_settings(cls):
+        return cls(**settings.REDIS_SETTINGS)
+
+    def _push_set(self, list_name, *value):
+        self.conn.sadd(list_name, *value)
+
+    def add_raw(self, *values):
+        self._push_set(RAW_IPS, *values)
+
+    def add_http(self, *values):
+        self._push_set(VALIDATED_HTTP, *values)
+
+    def add_https(self, *values):
+        self._push_set(VALIDATED_HTTPS, *values)
+
+    def add_validated(self, *values):
+        for ip in values:
+            if ip.startswith('https'):
+                self.add_https(ip)
+            elif ip.startswith('http'):
+                self.add_http(ip)
+            else:
+                pass
+
+    def _iter(self, ip_type):
+        for ip in self.conn.sscan_iter(ip_type):
+            yield ip.decode()
+            self.conn.srem(ip_type, ip)
+
+    def raw_iter(self):
+        yield from self._iter(RAW_IPS)
+
+    def http_iter(self):
+        yield from self._iter(VALIDATED_HTTP)
+
+    def https_iter(self):
+        yield from self._iter(VALIDATED_HTTPS)
+
+
+db = DB.from_settings()
