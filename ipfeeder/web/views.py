13e25f98a4f916a98c06b4f39c13e21065f4246f
fucangyu <cangyufu@gmail.com>
2019-03-10 04:40:34 +0800

    change api

--- ipfeeder/web/views.py
+++ ipfeeder/web/views.py
@@ -1,10 +1,30 @@
-from flask import Flask, jsonify
+from typing import Tuple
+from flask import Flask, jsonify, request
 from ipfeeder.db import db
 
 app = Flask('__name__')
 
 
-@app.route('/', methods=['GET'])
+def validate(kwargs: dict) -> Tuple[bool, str]:
+    try:
+        n = kwargs.get('n')
+        if n:
+            int(n)
+    except ValueError:
+        return False, 'The type of n must be int.'
+    protocol = kwargs.get('protocol')
+    if protocol and protocol not in ['http', 'https']:
+        return False, 'The protocol must be http or https.'
+    return True, ''
+
+
+@app.route('/proxies/', methods=['GET'])
 def proxies():
-    result = db.to_dict()
-    return jsonify(result)
+    args = request.args.to_dict()
+    validated, msg = validate(args)
+    n = args.get('n')
+    data = db.to_dict(
+        n=int(n) if n else None,
+        protocol=args.get('protocol'),
+    ) if validated else {}
+    return jsonify(dict(msg=msg, data=data)), 200 if validated else 400
